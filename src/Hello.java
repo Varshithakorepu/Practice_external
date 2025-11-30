9434d5c3bf8ed943aca567600abece2d06502a53
Varshitha Korepu <varshithakorepu1105@gmail.com>
2025-11-30 12:24:10 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello! welcome to DevOps");
+        System.out.println("Hello! ");
         System.out.println("Hii Webhooks");
     }
 }
